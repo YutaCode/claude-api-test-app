92864048ca866ac9aab1dabdb4973248c4b5efcf
yuta <ytknknorm820@gmail.com>
2024-04-25 20:35:59 +0900

    add app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-
+import base64
 import anthropic
 from dotenv import load_dotenv
 
@@ -9,12 +9,16 @@ client = anthropic.Anthropic(
     api_key=os.environ.get("ANTHROPIC_API_KEY"),
 )
 
+with open("image.jpg", "rb") as image:
+    encoded_image = base64.b64encode(image.read()).decode("utf-8")
+
 message = client.messages.create(
     model="claude-3-opus-20240229",
     max_tokens=1000,
     temperature=0,
     messages=[
-        {"role": "user", "content": "Hello!"}
+        {"role": "user",
+         "content": f"Here is an image: <image>{encoded_image}>\nWhat does image look like?"},
     ]
 )
-print(message.content)
+print(message.content[0].text)
